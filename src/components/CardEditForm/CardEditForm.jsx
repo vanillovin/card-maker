dbc92bbd4728c8da87d0f910d31fb7e342274652
vanillovin <vanillovin@gmail.com>
2022-04-13 19:55:42 +0900

    add card and edit form component

--- src/components/CardEditForm/CardEditForm.jsx
+++ src/components/CardEditForm/CardEditForm.jsx
@@ -1,10 +1,77 @@
 import React, { memo, useRef } from 'react';
+import Button from '../Button/Button';
+import ImageFileInput from '../ImageFileInput/ImageFileInput';
 import styles from './CardEditForm.module.css';
 
 const CardEditForm = memo(({ card }) => {
+  const nameRef = useRef();
+  const companyRef = useRef();
+  const themeRef = useRef();
+  const titleRef = useRef();
+  const emailRef = useRef();
+  const messageRef = useRef();
+
+  const { name, company, title, email, message, theme, fileName } = card;
+
+  const onChange = () => {};
+  const onSubmit = () => {};
+
   return (
     <form className={styles.form}>
-     
+      <input
+        className={styles.input}
+        type="text"
+        name="name"
+        ref={nameRef}
+        value={name}
+        onChange={onChange}
+      />
+      <input
+        className={styles.input}
+        type="text"
+        name="company"
+        ref={companyRef}
+        value={company}
+        onChange={onChange}
+      />
+      <select
+        className={styles.select}
+        name="theme"
+        ref={themeRef}
+        value={theme}
+        onChange={onChange}
+      >
+        <option value="light">light</option>
+        <option value="dark">dark</option>
+        <option value="colorful">colorful</option>
+      </select>
+      <input
+        className={styles.input}
+        type="text"
+        name="title"
+        ref={titleRef}
+        value={title}
+        onChange={onChange}
+      />
+      <input
+        className={styles.input}
+        type="text"
+        name="email"
+        ref={emailRef}
+        value={email}
+        onChange={onChange}
+      />
+      <textarea
+        className={styles.textarea}
+        ref={messageRef}
+        name="message"
+        value={message}
+        onChange={onChange}
+      />
+      <div className={styles.fileInput}>
+        <ImageFileInput />
+      </div>
+      <Button name="Delete" onClick={onSubmit} />
     </form>
   );
 });
---
+++ src/components/ImageFileInput/ImageFileInput.jsx
@@ -0,0 +1,11 @@
+import React, { memo, useRef, useState } from 'react';
+import styles from './ImageFileInput.module.css';
+
+const ImageFileInput = memo(() => {
+  return (
+    <div className={styles.container}>
+    </div>
+  );
+});
+
+export default ImageFileInput;
